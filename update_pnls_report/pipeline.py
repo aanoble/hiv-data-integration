e2c666554ee57e972035f02227d8c563b577863c
arnaudanoble17 <aanoble@bluesquarehub.com>
2025-06-30 11:30:50 +0000

    previous integration pipelines

--- update_pnls_report/pipeline.py
+++ update_pnls_report/pipeline.py
@@ -118,11 +118,11 @@ def consolidate_dhis2_and_naomi_data(
     Returns:
         A Polars DataFrame containing the fetched data.
     """
-    # df_naomi = extract_naomi_api_data(year=annee_extraction, fp_ressources=fp_ressources)
+    df_naomi = extract_naomi_api_data(year=annee_extraction, fp_ressources=fp_ressources)
 
-    df_naomi = pl.read_parquet(
-        Path(workspace.files_path, f"data/données spectrum/naomi_data_{annee_extraction}.parquet")
-    )
+    # df_naomi = pl.read_parquet(
+    #     Path(workspace.files_path, f"data/données spectrum/naomi_data_{annee_extraction}.parquet")
+    # )
 
     coc = pl.DataFrame(dhis2.meta.category_option_combos())
     organisation_units = pl.DataFrame(dhis2.meta.organisation_units())
